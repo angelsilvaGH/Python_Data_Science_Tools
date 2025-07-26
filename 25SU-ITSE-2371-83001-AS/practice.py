9b56b85b45a6fd03d99314358248edbe2263fc64
angel silva <angelsilva@macbookpro.attlocal.net>
2025-07-26 17:27:38 -0500

    Commit 0

---
+++ 25SU-ITSE-2371-83001-AS/practice.py
@@ -0,0 +1,9 @@
+# import libraries
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+from sklearn.impute import SimpleImputer
+# import the dataset
+df= pd.read_csv("review_dataset.csv")
+# Print the dataset shape (rows/columns)
+print("The shape of the dataset is:", df.shape)
